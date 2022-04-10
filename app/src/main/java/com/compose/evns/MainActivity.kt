f326a02eb2b892dbb20332934b2c07c2be2f8aa3
nthnua <97590341+nthnua@users.noreply.github.com>
2022-04-10 19:02:38 +0530

    Initial message card implementation

--- app/src/main/java/com/compose/evns/MainActivity.kt
+++ app/src/main/java/com/compose/evns/MainActivity.kt
@@ -3,11 +3,11 @@ package com.compose.evns
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.compose.evns.ui.elements.Message
+import com.compose.evns.ui.elements.MessageCard
 import com.compose.evns.ui.theme.EvnsTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +15,35 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             EvnsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Scaffold() {
+                    // A surface container using the 'background' color from the theme
+                    Surface(color = MaterialTheme.colors.background) {
+                        MessageCard(Message("Admin","Hellooo."))
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     EvnsTheme {
-        Greeting("Android")
+        MessageCard(Message("Admin","Hello everyone \n" +
+                "Want to display ipl heroics , then put on your helmets and March towards  the pitch\n" +
+                "\n" +
+                "Inter Branch Mens Cricket Match \uD83C\uDFCF\uD83C\uDFCFis been conducted this year.\n" +
+                "Interested students  of CSE branch do join the below mentioned WhatsApp group \n" +
+                "Futher information will be provided in group itself\n" +
+                "https://chat.whatsapp.com/I18vdJDFSfQHfNGBnu3fpq\n" +
+                "\n" +
+                "So till then practice ur shots and precise ur line and lengths\n" +
+                "\n" +
+                "Contact details\n" +
+                "Mohith : 70198 99149\n" +
+                "Preeth Jain : 6366299788\n" +
+                "Nishan : 93536 26903"))
+
     }
 }
---
+++ app/src/main/java/com/compose/evns/ui/elements/MessageCard.kt
@@ -0,0 +1,38 @@
+package com.compose.evns.ui.elements
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.compose.evns.R
+
+data class Message(val author:String, val body:String)
+
+@Composable
+fun MessageCard(msg: Message){
+    Card(modifier = Modifier.padding(all = 8.dp).fillMaxWidth(), backgroundColor = Color.LightGray, shape = RoundedCornerShape(8.dp)) {
+
+        Row(modifier = Modifier.padding(all = 4.dp) ){
+            Image(painter = painterResource(R.drawable.memoji),
+                contentDescription = "Memoji :)",
+                modifier = Modifier.size(40.dp).clip(CircleShape)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Column{
+                Text("${msg.author}", modifier = Modifier.height(40.dp), fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                Spacer(modifier = Modifier.height(20.dp))
+                Text("${msg.body}")
+            }
+        }
+    }
+}
