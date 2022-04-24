63c3bf2d81fa75d8658eea64840207eb322927c2
a-star12601 <95485054+a-star12601@users.noreply.github.com>
2022-04-24 10:46:56 +0530

    anuroop changes1

--- app/src/main/java/com/compose/evns/MainActivity.kt
+++ app/src/main/java/com/compose/evns/MainActivity.kt
@@ -1,26 +1,65 @@
 package com.compose.evns
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.compose.evns.ui.elements.Message
 import com.compose.evns.ui.elements.MessageCard
 import com.compose.evns.ui.theme.EvnsTheme
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import java.util.Date
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        val db = Firebase.firestore
         setContent {
             EvnsTheme {
-                Scaffold() {
+                Scaffold(
+                    floatingActionButton = {
+                        ExtendedFloatingActionButton(
+                            text = { Text("Add test") },
+                            onClick = {
+                                val user = hashMapOf(
+                                    "first" to "Ada",
+                                    "last" to "Lovelace",
+                                    "born" to 1815
+                                )
+
+                                db.collection("test")
+                                    .add(user)
+                                    .addOnSuccessListener { documentReference ->
+                                        Toast.makeText(this@MainActivity, "Success", Toast.LENGTH_SHORT).show()
+                                    }
+                                    .addOnFailureListener { e ->
+                                        Toast.makeText(this@MainActivity, "Fail", Toast.LENGTH_SHORT).show()
+                                    }}
+                        )
+                    },
+                ) {
                     // A surface container using the 'background' color from the theme
                     Surface(color = MaterialTheme.colors.background) {
-                        MessageCard(Message("Admin","Hellooo.",Date().toString()))
+                        //MessageCard(Message("Admin","Hellooo.",Date().toString()))
+                        Button(
+                            onClick = { Fetch(db,this) },
+                            // Uses ButtonDefaults.ContentPadding by default
+                            contentPadding = PaddingValues(
+                                start = 20.dp,
+                                top = 12.dp,
+                                end = 20.dp,
+                                bottom = 12.dp
+                            )){
+                            // Inner content including an icon and a text label
+                            Text("View")
+                        }
                     }
                 }
             }
@@ -49,4 +88,17 @@ fun DefaultPreview() {
             time = Date().toString()))
 
     }
+}
+
+fun Fetch(db : FirebaseFirestore,activity: MainActivity){
+    db.collection("test")
+        .get()
+        .addOnSuccessListener { result ->
+            for (document in result) {
+                Toast.makeText(activity, "${document.id} => ${document.data}", Toast.LENGTH_SHORT).show()
+            }
+        }
+        .addOnFailureListener { exception ->
+            Toast.makeText(activity, "Fail", Toast.LENGTH_SHORT).show()
+        }
 }
